d075605ff9f386803fd81e98c150f1a2e7426c6b
Kaia B <kaiab@Kaias-MacBook-Air.local>
2026-03-09 23:28:46 -0700

    hi

--- lol.py
+++ lol.py
@@ -18,9 +18,12 @@ def classify_data(name):
 
     return data.X, data.y, data.n_samples, data.n_features
 
+#test
+"""
 if __name__=="__main__":
     X,y,n_samples, n_features = classify_data("olivetti")
     print("X shape:", X.shape)
     print("y shape:", y.shape)
     print("n_samples:", n_samples)
     print("n_features:", n_features)
+    """
