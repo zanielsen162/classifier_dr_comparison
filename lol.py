8650ded482d4eb3de4d6a78be1333e8aa3f80f21
Kaia B <kaiab@Kaias-MacBook-Air.local>
2026-03-09 23:14:26 -0700

    hi

---
+++ lol.py
@@ -0,0 +1,26 @@
+from data import(load_olivetti, load_newsgroups, load_fashion_minst, load_minst)
+
+def classify_data(name):
+    name = name.upper()
+
+    if name == "OLIVETTI":
+        data = load_olivetti()
+
+    elif name == "NEWSGROUPS":
+        data = load_newsgroups()
+
+    elif name == "FASHION_MINST":
+        data = load_fashion_minst()
+
+    elif name == "MINST":
+        data = load_minst()
+    
+
+    return data.X, data.y, data.n_samples, data.n_features
+
+if __name__=="__main__":
+    X,y,n_samples, n_features = classify_data("olivetti")
+    print("X shape:", X.shape)
+    print("y shape:", y.shape)
+    print("n_samples:", n_samples)
+    print("n_features:", n_features)
