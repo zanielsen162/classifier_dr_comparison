3d5346f2645321745187f842ef53d96bdfe5dad9
Kaia B <kaiab@169-231-205-166.wireless.ucsb.edu>
2026-03-10 12:00:56 -0700

    hi

--- lol.py
+++ lol.py
@@ -1,6 +1,12 @@
-from data import(load_olivetti, load_newsgroups, load_fashion_minst, load_minst)
+import numpy as numpy
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 
-def classify_data(name):
+
+from data import load_olivetti, load_newsgroups, load_fashion_minst, load_minst
+
+def load_data(name):
     name = name.upper()
 
     if name == "OLIVETTI":
@@ -15,15 +21,37 @@ def classify_data(name):
     elif name == "MINST":
         data = load_minst()
     
+    return data
 
-    return data.X, data.y, data.n_samples, data.n_features
+def KNN_traindata(name):
+    data = load_data(name)
+    kkn = KNeighborsClassifier(n_neighbors = 3)
+    knn.fit(data.X, data.y)
+    return knn.predict(data.X)
 
-#test
-"""
-if __name__=="__main__":
-    X,y,n_samples, n_features = classify_data("olivetti")
-    print("X shape:", X.shape)
-    print("y shape:", y.shape)
-    print("n_samples:", n_samples)
-    print("n_features:", n_features)
-    """
+
+def logreg_traindata(name):
+    data = load_data(name)
+    logreg = LogisticRegression(solver = 'liblinear', max_iter=1000)
+    logreg.fit(data.X, data.y)
+    return logreg.predict(data.X)
+
+
+def naive_traindata(name):
+    data = load_data(name)
+    naive = GaussianNB()
+    naive.fit(data.X, data.y)
+    return naive.predict(data.X)
+
+
+
+
+#data.py passes me the data
+#this file should classify the data by matching the data in data.py to the format used for the true labels so we can compare the labels in the next file
+#should output something that matches the format of data.y
+
+#train each of the following data sets on the original features ^^
+
+#k-NN
+#logistic regression
+#naive bayes
